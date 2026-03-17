e1f9805ecdccbc26697d211be38a845dc5471315
bisaio <129079375+bisaio@users.noreply.github.com>
2026-03-17 00:20:18 -0300

    FEAT: add react-router-dom na main e app

--- bulletin_board/src/App.tsx
+++ bulletin_board/src/App.tsx
@@ -1,10 +1,13 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Bulletin board</h1>
-    </>
+    <div className='App'>
+      <div className='container'>
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
--- bulletin_board/src/main.tsx
+++ bulletin_board/src/main.tsx
@@ -3,8 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import Home from './routes/Home.tsx'
+import NewPage from './routes/NewPost.tsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: "/new-post",
+        element: <NewPage />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
